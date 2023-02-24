ebd07d7f6a0e64df9d06c5e8d0240aaba685d459
philipperbd <philippe.rambaud@sogeti.com>
2023-02-24 17:21:00 +0100

    handle red arrows

--- shap_on_image/ShapOnImageAuto.py
+++ shap_on_image/ShapOnImageAuto.py
@@ -43,6 +43,39 @@ class ShapOnImageAuto:
         """
         return self.positions
 
+    def symmetry(self, plot_name):
+        """
+        return value of symmetry for top/bot and left/right
+        """
+        check_list_top = ['shoulder', 'elbow', 'wrist', 'top']
+        check_list_right = ['rshoulder', 'relbow', 'rwrist', 'rhip', 'rknee', 'rankle', 'right']
+        
+        top, bot, right, left = [], [], [], []
+        
+        for feature, shap_value in self.shap[plot_name].items():
+            for word in check_list_top:
+                if word in feature.lower(): top.append(shap_value)
+                else: bot.append(shap_value)
+            for word in check_list_right:
+                if word in feature.lower(): right.append(shap_value)
+                else: left.append(shap_value)
+        
+        print(plot_name, sum(top), sum(bot), sum(right), sum(left))
+
+        top, bot, right, left = sum(top), sum(bot), sum(right), sum(left)
+        
+        if bot > top:
+            sym_top_bot = round(bot / top)
+        else:
+            sym_top_bot = round(top / bot) * -1
+        
+        if left > right:
+            sym_left_right = round(left / right)
+        else:
+            sym_left_right = round(right / left) * -1
+
+        return sym_top_bot, sym_left_right
+
     def create_plot(self, path, plot_name, suptitle="", title="", alpha=1):
         """
         Plot the figure with image and shap values at specific positions
@@ -55,14 +88,14 @@ class ShapOnImageAuto:
         fig, ax = plt.subplots()
         im = ax.imshow(im)
         plt.axis('off')
+
         try:
             dataset = plot_name.rsplit('_', 1)[0]
-            auc = self.auc[dataset]['auc']
+            auc = self.auc[dataset]['mean']
             std = self.auc[dataset]['std']
         except:
             auc = 00
             std = 00
-
 
         plt.suptitle(plot_name.replace('_', ' ').upper(), weight="bold")
         plt.title('XGBoost mean AUC : ' + str(auc) + ' ('+ str(std) + ')')
@@ -76,7 +109,10 @@ class ShapOnImageAuto:
                 plt.scatter(x, y, s=abs(shap), color=color)
             except:
                 print(plot_name, feature)
-                
+
+        sym_top_bot, sym_left_right = self.symmetry(plot_name=plot_name)
+        plt.arrow(400, 410, sym_left_right * 3, 0, head_width=5, color="crimson")
+        plt.arrow(400, 410, 0, sym_top_bot * 3, head_width=5, color="crimson")
 
         plt.close(fig)
         fig.savefig(path + plot_name + '.png')
