bc9bd64ec7e9616160226c45a9da1c6e21dd63e4
philipperbd <philippe.rambaud@sogeti.com>
2023-03-01 10:22:14 +0100

    refacto lib

--- shap_on_image/ShapOnImageAuto.py
+++ shap_on_image/ShapOnImageAuto.py
@@ -3,6 +3,7 @@ import os
 import cv2
 from shap_on_image.utils import *
 
+
 class ShapOnImageAuto:
 
     def __init__(self, image, features, shap, auc, positions={}):
@@ -12,18 +13,19 @@ class ShapOnImageAuto:
             features: list, features names as strings
             shap: list, shap values extracted from shap packages
             positions: dict, features with 'x' and 'y' values, can be set with set_positions()
-        """       
-        try: 
+        """
+        try:
             os.path.exists(image)
             self.image = image
             self.features = features
-            self.shap = shap 
+            # self.values = values
+            self.shap = shap
             self.auc = auc
             self.positions = positions
             self.feature_cnt = 0
         except:
             print('Error during init - verify path to image')
-    
+
     def set_positions(self):
         """
         Tool to select positions of every feature on the image.
@@ -43,66 +45,7 @@ class ShapOnImageAuto:
         """
         return self.positions
 
-    def symmetry(self, plot_name):
-        """
-        return value of symmetry for top/bot and left/right
-        """
-        check_list_top = [
-            'lshoulder-lelbow', 'rshoulder-relbow', 'lelbow-lwrist', 'relbow-rwrist',
-            'lshoulder', 'rshoulder', 'lelbow', 'relbow', 'lwrist', 'rwrist',
-            'top_right', 'top_left']
-        check_list_right = [
-            'rshoulder-relbow', 'relbow-rwrist', 'rshoulder', 'relbow', 'rwrist',
-            'rhip-rknee', 'rknee-rankle', 'rhip', 'rknee', 'rankle',
-            'top_right', 'bot_right']
-        
-        
-        top, bot, right, left = [], [], [], []
-        nb_top, nb_bot, nb_right, nb_left = 0, 0, 0, 0
-
-        print(plot_name)
-        
-        for feature, shap_value in self.shap[plot_name].items():
-            if feature == "Face":
-                continue
-            if plot_name[:11] == 'linear_data':
-                feature = feature[:-2]
-            if feature.lower() in check_list_top:
-                #print("top", feature)
-                top.append(shap_value)
-                nb_top += 1
-            else: 
-                #print("bot", feature)
-                bot.append(shap_value)
-                nb_bot += 1
-            if feature.lower() in check_list_right:
-                #print("right", feature)
-                right.append(shap_value)    
-                nb_right += 1
-            else: 
-                #print("left", feature)
-                left.append(shap_value)
-                nb_left += 1
-
-        top, bot, right, left = sum(top), sum(bot), sum(right), sum(left)
-
-        if bot > top:
-            sym_top_bot = round((bot / top), 2)
-        else:
-            sym_top_bot = round((top / bot), 2)
-        if right > left:
-            sym_left_right = round((right / left), 2) * -1
-        else:
-            sym_left_right = round((left / right), 2)
-
-        print(top, bot, left, right)
-        print(sym_top_bot, sym_left_right)
-
-        print('--------------')
-
-        return sym_top_bot, sym_left_right
-
-    def create_plot(self, path, plot_name, suptitle="", title="", alpha=1):
+    def create_plot(self, path, plot_name, suptitle, title, sym, alpha=1):
         """
         Plot the figure with image and shap values at specific positions
         args:
@@ -116,44 +59,76 @@ class ShapOnImageAuto:
         plt.axis('off')
         print_lines = False
 
-        dataset = plot_name[:plot_name.rfind('_')]
-
-        mean = round(self.auc[dataset]['mean'], 2)
-        std = round(self.auc[dataset]['std'], 2)
-        sym_top_bot, sym_left_right = self.symmetry(plot_name=plot_name)
-
-        plt.suptitle(plot_name.replace('_', ' ').upper(), weight="bold")
-        plt.title(
-            'AUC: ' + str(mean) + '('+ str(std) + ') ' + \
-            'L/R: ' + str(sym_left_right) + ' ' + \
-            'T/B: ' + str(-sym_top_bot))
+        plt.suptitle(suptitle, weight="bold")
+        plt.title(title)
 
         for feature, shap_value in self.shap[plot_name].items():
             shap = shap_value * alpha
             color = 'blue'
-            
-            if plot_name[:11] == 'linear_data' and feature != "Face":
-                feature_type = feature[-2:]
-                x = self.positions[feature[:-2]]['x']
-                y = self.positions[feature[:-2]]['y']
-                shap = shap / 7 
 
-                if feature_type == '_x':
-                    plt.plot([x - abs(shap)/2, x + abs(shap)/2], [y, y], color='blue')
-                else:
-                    plt.plot([x, x], [y - abs(shap)/2, y + abs(shap)/2], color='blue')
-            else:
-                x = self.positions[feature]['x']
-                y = self.positions[feature]['y']
-                plt.scatter(x, y, s=abs(shap), color=color)
+            x = self.positions[feature]['x']
+            y = self.positions[feature]['y']
+            plt.scatter(x, y, s=abs(shap), color=color)
 
-        plt.arrow(202, 221, sym_left_right * 10, 0, head_width=5, color="crimson")
-        plt.arrow(202, 221, 0, sym_top_bot * 10, head_width=5, color="crimson")
+        plt.arrow(202, 221, 0, sym[0] * 10,
+                  head_width=5, color="crimson")  # Top/Bot
+        plt.arrow(202, 221, sym[1] * 10, 0, head_width=5,
+                  color="crimson")  # Left/Right
 
         plt.close(fig)
         fig.savefig(path + plot_name + '.png')
 
-    def create_plots(self, path = "", alpha=1):
+    def create_plot_linear(self, path, plot_name, suptitle, title, sym, alpha=1):
+        """
+        Plot the figure with image and shap values at specific positions
+        args:
+            suptitle: str, suptitle to display on image
+            title: str, title to display on image
+            alpha: float, coefficient to multiply every shap values by.
+        """
+        im = plt.imread(self.image)
+        fig, ax = plt.subplots()
+        im = ax.imshow(im)
+        plt.axis('off')
+        print_lines = False
+
+        plt.suptitle(suptitle, weight="bold")
+        plt.title(title)
+
+        print(plot_name)
+
+        for feature, shap_value in self.shap[plot_name].items():
+            print(feature)
+            shap = shap_value * alpha
+            color = 'blue'
+
+            if feature == "Face":
+                x = self.positions[feature]['x']
+                y = self.positions[feature]['y']
+                plt.scatter(x, y, s=abs(shap), color=color)
+
+            else:
+                feature_type = feature[-2:]
+                x = self.positions[feature[:-2]]['x']
+                y = self.positions[feature[:-2]]['y']
+                shap = shap / 7
+
+                if feature_type == '_x':
+                    plt.plot([x - abs(shap)/2, x + abs(shap)/2],
+                             [y, y], color=color)
+                else:
+                    plt.plot([x, x], [y - abs(shap)/2,
+                             y + abs(shap)/2], color=color)
+
+        plt.arrow(202, 221, 0, sym[0] * 10,
+                  head_width=5, color="crimson")  # Top/Bot
+        plt.arrow(202, 221, sym[1] * 10, 0, head_width=5,
+                  color="crimson")  # Left/Right
+
+        plt.close(fig)
+        fig.savefig(path + plot_name + '.png')
+
+    def create_plots(self, path="", alpha=1):
         """Create several plots and save them in path.
 
         Args:
@@ -161,7 +136,15 @@ class ShapOnImageAuto:
             alpha (int, optional): Coefficient to multiply shap values by. Defaults to 1.
         """
         for plot_name, _ in self.shap.items():
-            #print(plot_name)
-            self.create_plot(path, plot_name, alpha=alpha)
-        
-        
+            suptitle, title, sym = informations(
+                self, plot_name=plot_name, auc=self.auc)
+            if plot_name[:11] == 'linear_data':
+                self.create_plot_linear(
+                    path=path, plot_name=plot_name,
+                    suptitle=suptitle, title=title, 
+                    sym=sym, alpha=alpha)
+            else:
+                self.create_plot(
+                    path=path, plot_name=plot_name,
+                    suptitle=suptitle, title=title, 
+                    sym=sym, alpha=alpha)
--- shap_on_image/utils.py
+++ shap_on_image/utils.py
@@ -6,7 +6,8 @@ def add_shap_to_values(self):
     id = 0
     for key, _ in self.values.items():
         self.values[key]['shap'] = self.shap[id]
-        id +=1
+        id += 1
+
 
 def ask_for_feature(self):
     """
@@ -14,7 +15,7 @@ def ask_for_feature(self):
     """
     print('Positions for', self.features[self.feature_cnt], end=" ")
     self.feature_cnt += 1
-    
+
 
 def click_event(event, x, y, flags, params):
     """
@@ -28,13 +29,79 @@ def click_event(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(img, str(x) + ',' +
-                    str(y), (x,y), font,
+                    str(y), (x, y), font,
                     1, (255, 0, 0), 2)
         cv2.imshow('image', img)
-        self.positions[self.features[self.feature_cnt-1]] = {'x' : x, 'y': y}
+        self.positions[self.features[self.feature_cnt-1]] = {'x': x, 'y': y}
         print(x, y)
         if self.feature_cnt == len(self.features):
             cv2.destroyAllWindows()
-        else: 
+        else:
             ask_for_feature(self)
-            
+
+def symmetry(self, plot_name):
+    """
+    return value of symmetry for top/bot and left/right
+    """
+    check_list_top = [
+        'lshoulder-lelbow', 'rshoulder-relbow', 'lelbow-lwrist', 'relbow-rwrist',
+        'lshoulder', 'rshoulder', 'lelbow', 'relbow', 'lwrist', 'rwrist',
+        'top_right', 'top_left']
+    check_list_right = [
+        'rshoulder-relbow', 'relbow-rwrist', 'rshoulder', 'relbow', 'rwrist',
+        'rhip-rknee', 'rknee-rankle', 'rhip', 'rknee', 'rankle',
+        'top_right', 'bot_right']
+
+    top, bot, right, left = [], [], [], []
+    nb_top, nb_bot, nb_right, nb_left = 0, 0, 0, 0
+
+    for feature, shap_value in self.shap[plot_name].items():
+        if feature == "Face":
+            continue
+        if plot_name[:11] == 'linear_data':
+            feature = feature[:-2]
+        if feature.lower() in check_list_top:
+            top.append(shap_value)
+            nb_top += 1
+        else:
+            bot.append(shap_value)
+            nb_bot += 1
+        if feature.lower() in check_list_right:
+            right.append(shap_value)
+            nb_right += 1
+        else:
+            left.append(shap_value)
+            nb_left += 1
+
+    top, bot, right, left = sum(top), sum(bot), sum(right), sum(left)
+
+    if bot > top:
+        sym_top_bot = round((bot / top), 2)
+    else:
+        sym_top_bot = round((top / bot), 2)
+    if right > left:
+        sym_left_right = round((right / left), 2) * -1
+    else:
+        sym_left_right = round((left / right), 2)
+
+    return [sym_top_bot, sym_left_right]
+
+
+def informations(self, plot_name, auc):
+    """ Return useful information for plots
+    """
+    dataset = plot_name[:plot_name.rfind('_')]
+
+    mean_auc = round(auc[dataset]['mean'], 2)
+    std_auc = round(auc[dataset]['std'], 2)
+
+    sym = symmetry(self, plot_name=plot_name)
+
+    suptitle = plot_name.replace('_', ' ').upper()
+
+    title = (
+        'AUC: ' + str(mean_auc) + '(' + str(std_auc) + ') ' +
+        'L/R: ' + str(sym[0]) + ' ' +
+        'T/B: ' + str(-sym[1]))
+
+    return suptitle, title, sym
