d8362c508e33c4dd5652dac72f082a4f7df55ee4
philipperbd <philippe.rambaud@sogeti.com>
2023-03-02 17:13:44 +0100

    error handling

--- launcher.sh
+++ launcher.sh
@@ -2,42 +2,72 @@ NOCOLOR='\033[0m'
 BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
-
+RED='\033[0;31m'
 DONE="${GREEN}done${NOCOLOR}"
+ERROR="${RED}error${NOCOLOR}"
+
+echo "${HELLO}"
 
 echo "${BLUE}START${NOCOLOR}"
 
-source venv/bin/activate
-echo "Virtual environment ${YELLOW}activated${NOCOLOR}"
+if ! source venv/bin/activate; then
+     echo "Virtual environment activation: ${ERROR}"
+else
+     echo "Virtual environment ${YELLOW}activated${NOCOLOR}"
+fi 2>/dev/null
 
-rm -rf data/$1/baby_visuals
-rm -rf data/$1/stats_plots
-rm data/$1/shap.json data/$1/shap_scalled.json data/$1/stats.json
-echo "Cleaning ${DONE}"
+if ! rm -rf data/$1/baby_visuals data/$1/stats_plots data/$1/shap.json data/$1/shap_scalled.json data/$1/stats.json;  then
+    echo "Cleaning: ${ERROR}"
+else
+    echo "Cleaning: ${DONE}"
+fi 2>/dev/null
 
-mkdir data/$1/baby_visuals
-mkdir data/$1/stats_plots
-echo "Init new directories: ${DONE}"
+if ! mkdir data/$1/baby_visuals data/$1/stats_plots; then
+    echo "Init new directories: ${ERROR}"
+else
+    echo "Init new directories: ${DONE}"
+fi 2>/dev/null
 
-python scripts/shap_dict.py --input data/$1/$2 --output data/$1/shap.json
-echo "Create dictionnary with shap values: ${DONE}"
+if ! python scripts/shap_dict.py --input data/$1/$2 --output data/$1/shap.json; then
+     echo "Create dictionnary with shap values: ${ERROR}"
+else
+     echo "Create dictionnary with shap values: ${DONE}"
+fi 2>/dev/null
 
-python scripts/minmaxscaler.py --input data/$1/shap.json
-echo "Min-max scaler on shap values: ${DONE}"
+if ! python scripts/minmaxscaler.py --input data/$1/shap.json &> /dev/null; then
+     echo "Min-max scaler on shap values: ${ERROR}"
+else
+     echo "Min-max scaler on shap values: ${DONE}"
+fi 2>/dev/null
 
-python scripts/stats_dict.py --path data/$1/
-echo "Create dictionnary with statistics: ${DONE}"
+if ! python scripts/stats_dict.py --path data/$1/ &> /dev/null; then
+     echo "Create dictionnary with statistics: ${ERROR}"
+else
+     echo "Create dictionnary with statistics: ${DONE}"
+fi 2>/dev/null
 
-python scripts/stats_plots.py --path data/$1/
-echo "Generate statistics plots: ${DONE}"
+if ! python scripts/stats_plots.py --path data/$1/ &> /dev/null; then
+     echo "Generate statistics plots: ${ERROR}"
+else
+     echo "Generate statistics plots: ${DONE}"
+fi 2>/dev/null
 
-python scripts/visuals.py --path data/$1 --image data/baby.png
-echo "Generate baby visuals: ${DONE}"
+if ! python scripts/visuals.py --path data/$1 --image data/baby.png &> /dev/null; then
+     echo "Generate baby visuals: ${ERROR}"
+else
+     echo "Generate baby visuals: ${DONE}"
+fi 2>/dev/null
 
-zip -r reports/$1_$2.zip data/$1/stats_plots data/$1/baby_visuals &> /dev/null
-echo "Add every visuals and plots to a report folder: ${DONE}"
+if ! zip -r reports/$1_$2.zip data/$1/stats_plots data/$1/baby_visuals &> /dev/null; then
+     echo "Add every visuals and plots to a report folder: ${ERROR}"
+else
+     echo "Add every visuals and plots to a report folder: ${DONE}"
+fi 2>/dev/null
 
-deactivate
-echo "Virtual environment ${YELLOW}deactivated${NOCOLOR}"
+if ! deactivate &> /dev/null; then
+     echo "Virtual environment deactivation: ${ERROR}"
+else
+     echo "Virtual environment ${YELLOW}deactivated${NOCOLOR}"
+fi 2>/dev/null
 
 echo "${BLUE}END${NOCOLOR}"
