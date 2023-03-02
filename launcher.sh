a5815df1167d86560908f7c416109c74542208d5
philipperbd <philippe.rambaud@sogeti.com>
2023-03-02 16:39:40 +0100

    beautiful bash script with color outputs

--- launcher.sh
+++ launcher.sh
@@ -1,25 +1,43 @@
-echo "--Start--"
+NOCOLOR='\033[0m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+
+DONE="${GREEN}done${NOCOLOR}"
+
+echo "${BLUE}START${NOCOLOR}"
+
 source venv/bin/activate
-echo "Virtual environment activated."
+echo "Virtual environment ${YELLOW}activated${NOCOLOR}"
+
 rm -rf data/$1/baby_visuals
 rm -rf data/$1/stats_plots
 rm data/$1/shap.json data/$1/shap_scalled.json data/$1/stats.json
-echo "Cleaning done!"
+echo "Cleaning ${DONE}"
+
 mkdir data/$1/baby_visuals
 mkdir data/$1/stats_plots
-echo "New directories created at $1"
+echo "Init new directories: ${DONE}"
+
 python scripts/shap_dict.py --input data/$1/$2 --output data/$1/shap.json
-echo "shap.json done."
+echo "Create dictionnary with shap values: ${DONE}"
+
 python scripts/minmaxscaler.py --input data/$1/shap.json
-echo "shap_scalled.json done."
+echo "Min-max scaler on shap values: ${DONE}"
+
 python scripts/stats_dict.py --path data/$1/
-echo "stats.json done."
+echo "Create dictionnary with statistics: ${DONE}"
+
 python scripts/stats_plots.py --path data/$1/
-echo "stats visuals done."
+echo "Generate statistics plots: ${DONE}"
+
 python scripts/visuals.py --path data/$1 --image data/baby.png
-echo "baby visuals done."
+echo "Generate baby visuals: ${DONE}"
+
 zip -r reports/$1_$2.zip data/$1/stats_plots data/$1/baby_visuals &> /dev/null
-echo "report zip folder done."
+echo "Add every visuals and plots to a report folder: ${DONE}"
+
 deactivate
-echo "Virtual environment deactivated."
-echo "--End--"
+echo "Virtual environment ${YELLOW}deactivated${NOCOLOR}"
+
+echo "${BLUE}END${NOCOLOR}"
